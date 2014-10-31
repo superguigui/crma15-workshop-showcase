2ca84c663e919b52f68fe3345a57fd4f97720090
Clément Bardon <cba@octaveoctave.com>
2014-10-31 16:04:46 +0100

    Changed the logo and added some margins around

--- src/views/layout/header/header.js
+++ src/views/layout/header/header.js
@@ -9,6 +9,6 @@ module.exports = {
         var startTime = 0.8;
         var tl = new TimelineMax();
         tl.from(this.$$.subtitle, 0.6, {y: -20, alpha: 0, ease: Expo.easeOut}, startTime + 0.0);
-        tl.from(this.$$.title, 0.6, {y: -20, alpha: 0, ease: Expo.easeOut}, startTime + 0.1);
+        tl.from(this.$$.logo, 0.6, {y: -20, alpha: 0, ease: Expo.easeOut}, startTime + 0.1);
     }
 };
